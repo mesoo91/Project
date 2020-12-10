adc8d996354d839f7cf4e9ad541debce20ba0ba9
mesoo91 <mesoo91>
2020-12-10 20:09:22 +0900

    Practice3.java 전개도에 따른 인덱스화, 인덱스에 따른 배열의 실제 위치 설정

--- src/Practice3.java
+++ src/Practice3.java
@@ -1,20 +1,36 @@
+import javafx.util.Pair;
+
 import java.util.Scanner;
 
 public class Practice3 {
     static String[][] cube = { // 자주 사용하는 변수이므로 어쩔 수 없이 전역변수 사용
             {" ", " ", " ", "B", "B", "B", " ", " ", " ", " ", " ", " "},
             {" ", " ", " ", "B", "B", "B", " ", " ", " ", " ", " ", " "},
-            {" ", " ", " ", "B", "B", "B", " ", " ", " ", " ", " ", " "},
+            {" ", " ", " ", "B", "B", "B", " ", " ", " ", " ", " ", " "}, // 윗면 0
             {"W", "W", "W", "O", "O", "O", "G", "G", "G", "Y", "Y", "Y"},
             {"W", "W", "W", "O", "O", "O", "G", "G", "G", "Y", "Y", "Y"},
-            {"W", "W", "W", "O", "O", "O", "G", "G", "G", "Y", "Y", "Y"},
+            {"W", "W", "W", "O", "O", "O", "G", "G", "G", "Y", "Y", "Y"}, // 왼쪽 1, 정면 2, 오른쪽 3, 후면 4
             {" ", " ", " ", "R", "R", "R", " ", " ", " ", " ", " ", " "},
             {" ", " ", " ", "R", "R", "R", " ", " ", " ", " ", " ", " "},
-            {" ", " ", " ", "R", "R", "R", " ", " ", " ", " ", " ", " "}
+            {" ", " ", " ", "R", "R", "R", " ", " ", " ", " ", " ", " "} // 아랫면 5
     };
 
     public static void main(String[] args) { // 대다수 코드를 Practice2.java에서 가져옴
         Scanner scan = new Scanner(System.in);
+
+        int[][] related = new int[][] { // 연관되어 있는 면들
+                {0, 2, 5, 4}, // 윗면, 정면, 아랫면, 후면
+                {0, 3, 5, 1}, // 윗면, 오른쪽, 아랫면, 왼쪽
+                {2, 3, 4, 1}, // 정면, 오른쪽, 후면, 왼쪽
+        };
+
+        Pair[] points = new Pair[6];
+        points[0] = new Pair(3, 0);
+        points[1] = new Pair(0, 3);
+        points[2] = new Pair(3, 3);
+        points[3] = new Pair(6, 3);
+        points[4] = new Pair(9, 3);
+        points[5] = new Pair(3, 6);
 
         while(true) {
             System.out.print("CUBE > ");
@@ -43,6 +59,10 @@ public class Practice3 {
                 System.out.println();
             }
         }
+    }
+
+    public static void swap(int[] related, boolean left, int line) {
+        String[] str = new String[12];
     }
 
     public static void printCube() {
