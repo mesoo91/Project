a54f4b9553a1ad3ffb0ef20d41c37dc1c08701a3
mesoo91 <mesoo91>
2020-12-10 20:52:58 +0900

    Practice3.java 모든 상황에 적용하기 위해 각종 클래스 선언, 데이터 초기화

--- src/Practice3.java
+++ src/Practice3.java
@@ -1,5 +1,3 @@
-import javafx.util.Pair;
-
 import java.util.Scanner;
 
 public class Practice3 {
@@ -15,7 +13,7 @@ public class Practice3 {
             {" ", " ", " ", "R", "R", "R", " ", " ", " ", " ", " ", " "} // 아랫면 5
     };
 
-    static int[][] related = new int[][] { // 연관되어 있는 면들
+    static int[][] related = new int[][]{ // 연관되어 있는 면들
             {0, 2, 5, 4}, // 윗면, 정면, 아랫면, 후면
             {0, 3, 5, 1}, // 윗면, 오른쪽, 아랫면, 왼쪽
             {2, 3, 4, 1}, // 정면, 오른쪽, 후면, 왼쪽 U
@@ -32,30 +30,52 @@ public class Practice3 {
         points[4] = new Point(9, 3);
         points[5] = new Point(3, 6);
 
+        RelatedData horizontal = new RelatedData(); // horizontal = true
+        horizontal.relatedIndices = new int[] {2, 3, 4, 1}; // 정면, 오른쪽, 후면, 왼쪽 U D
+        horizontal.lines =            new boolean[] {false, false, false, false}; // 라인을 다른 데서 읽어야 하는가?
+        horizontal.directionChanged = new boolean[] {false, false, false, false}; // 방향이 달라지는가?
+        horizontal.reversed =         new boolean[] {false, false, true , false}; // 반대로 읽어야 하는가?
+
+        RelatedData verticalFront = new RelatedData(); // horizontal = false
+        verticalFront.relatedIndices = new int[] {0, 2, 5, 4};  // 윗면, 정면, 아랫면, 후면 L R
+        verticalFront.lines =            new boolean[] {false, false, false, true };
+        verticalFront.directionChanged = new boolean[] {false, false, false, false};
+        verticalFront.reversed =         new boolean[] {false, false, false, true };
+
+        RelatedData verticalSide = new RelatedData(); // horizontal = true, 윗면 메인
+        verticalSide.relatedIndices = new int[] {0, 3, 5, 1}; // 윗면, 오른쪽, 아랫면, 왼쪽 F B
+        verticalSide.lines =            new boolean[] {false, true , true, false};
+        verticalSide.directionChanged = new boolean[] {false, true, false, true};
+        verticalSide.reversed =         new boolean[] {false, false, true , true };
+
         Scanner scan = new Scanner(System.in);
 
-        // ** Y의 경우 (후면) 전개도 상으로는 뒤집어져야 하지만 뒤집어지지 않았다는 가정 하에 코드 작성 **
-
-        while(true) {
+        while (true) {
             System.out.print("CUBE > ");
             String input = scan.next();
 
-            if(input.equals("Q")) {
+            if (input.equals("Q")) {
                 System.out.println("Bye ~");
                 break;
             }
 
             String[] commands = Practice2.split(input); // Practice2의 코드를 그대로 사용
-            for(int i = 0; i < commands.length; i++) {
+            for (int i = 0; i < commands.length; i++) {
                 String command = commands[i];
 
                 boolean left = command.length() == 1;
 
-                switch(command.charAt(0)) {
-                    case 'U': swap(related[2], 0, true, left); break;
-                    case 'B': break;
-                    case 'L': break;
-                    case 'R': break;
+                switch (command.charAt(0)) {
+                    case 'U':
+                        break;
+                    case 'D':
+                        break;
+                    case 'L':
+                        break;
+                    case 'R':
+                        break;
+                    case 'B':
+                        break;
                 }
 
                 System.out.println(command);
@@ -65,47 +85,23 @@ public class Practice3 {
         }
     }
 
-    public static void swap(int[] related, int line, boolean horizontal, boolean left) {
+    public static void swap(RelatedData data, int overallLine, boolean overallDirection, boolean left) {
         String[] str = new String[12];
         String[] result = new String[12];
 
         int N = Practice1.getAdaptedIndex(left ? 3 : -3, 12); // Practice1의 코드 재사용 / 3개만큼 밀어내고, 12개를 기준으로 한다.
 
-
-        if(horizontal) {
-            int c = 0;
-            for(int i = 0; i < related.length; i++) { // 먼저 배열 후
-                Point point = points[related[i]];
-
-                for(int x = point.x; x < point.x + 3; x++) {
-                    str[c++] = cube[point.y + line][x];
-                }
-            }
-            int c2 = 0;
-            for(int i = N; i < 12; i++) { // 밀기
-                result[c2++] = str[i];
-            }
-            for(int i = 0; i < N; i++) {
-                result[c2++] = str[i];
-            }
-            int c3 = 0;
-            for(int i = 0; i < related.length; i++) { // 민 결과 반영
-                Point point = points[related[i]];
-
-//                System.out.println(point.x + ", " + point.y);
-
-                for(int x = point.x; x < point.x + 3; x++) {
-                    cube[point.y + line][x] = result[c3++];
-                }
-            }
-        } else {
-
+        for(int i = 0; i < 4; i++) {
+            int plate = data.relatedIndices[i];
+            int line = data.lines[i] ? 2 - overallLine : overallLine;
+            boolean reversed = data.reversed[i];
+            boolean directionChanged = data.directionChanged[i];
         }
     }
 
     public static void printCube() {
-        for(int y = 0; y < 9; y++) {
-            for(int x = 0; x < 12; x++) {
+        for (int y = 0; y < 9; y++) {
+            for (int x = 0; x < 12; x++) {
                 System.out.print(cube[y][x] + " ");
             }
             System.out.println();
@@ -121,4 +117,11 @@ class Point {
         x = x0;
         y = y0;
     }
+}
+
+class RelatedData {
+    int relatedIndices[];
+    boolean lines[];
+    boolean directionChanged[]; // 수평으로 읽는가? 수직으로 읽는가?
+    boolean reversed[]; // 반대로 읽어야 하는가?
 }
