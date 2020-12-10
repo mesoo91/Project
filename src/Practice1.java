763613bca74283a3e50dcf0486905d08edb2cbf5
mesoo91 <mesoo91>
2020-12-10 17:35:06 +0900

    배열에서 swap 코드 작성

--- src/Practice1.java
+++ src/Practice1.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Practice1 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
+        char[] text = scan.next().toCharArray();
+        int N = scan.nextInt();
+        String direction = scan.next(); // is L or R?
+    }
+
+    public static void swapAll(char[] text, boolean left) {
+        if(left) {
+
+        }
+    }
+
+    public static void swap(char[] text, int index1, int index2) {
+        char temp = text[index1];
+        text[index1] = text[index2];
+        text[index2] = temp;
     }
 }
