d4bb799742d652932a0f9dc0164bd4fd97f276af
mesoo91 <mesoo91>
2020-12-10 17:41:09 +0900

    swap 코드 삭제 while문을 통해 L에 대응하는 코드 작성

--- src/Practice1.java
+++ src/Practice1.java
@@ -4,20 +4,20 @@ public class Practice1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        char[] text = scan.next().toCharArray();
+        String text = scan.next();
         int N = scan.nextInt();
         String direction = scan.next(); // is L or R?
-    }
 
-    public static void swapAll(char[] text, boolean left) {
-        if(left) {
+        int c = 0;
 
+        while(c < text.length()) {
+            int index = c + N;
+            if(direction.equals("R")) {
+                index = text.length() - index;
+            }
+            System.out.print(index);
+            System.out.print(text.charAt(index % text.length()));
+            c++;
         }
-    }
-
-    public static void swap(char[] text, int index1, int index2) {
-        char temp = text[index1];
-        text[index1] = text[index2];
-        text[index2] = temp;
     }
 }
